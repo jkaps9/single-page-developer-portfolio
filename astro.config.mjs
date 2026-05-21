31eea968e4b02a7bc5ec9f1c922119f3e4f5049b
Josh <15219491+jkaps9@users.noreply.github.com>
2026-05-20 21:52:51 -0400

    create base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "", //TODO: update me
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Space Grotesk",
+      cssVariable: "--font-space-grotesk",
+      weights: [500, 700],
+    },
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "/src/styles/_variables.scss" as *;`,
+        },
+      },
+    },
+  },
+});
